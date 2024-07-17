ac6e69ab18fa0b20edfce08dee536c7cf27a9a82
Anfas K <100411700+Anfask@users.noreply.github.com>
2024-07-18 01:04:25 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,2 @@
+Sorry, it seems as if your user agent is not correct, in order to access this login Page. The one you supplied is: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0
+<!-- Thew3bxpl0rer  -->
